f9a91d01bd1a2d3a26b08cd3d8f2d4f6bec94c8d
Angi <amboutwe@users.noreply.github.com>
2021-08-18 13:28:26 -0600

    Add new review template
    For music albums. Includes sections for album listening and music videos.

--- pattern-template.php
+++ pattern-template.php
@@ -3,11 +3,13 @@
  * Template to copy for new ones.
  * Replace 'NAME', 'template-slug', 'TemplateName', 'TemplateDescription' and add content.
  */
+
 /*
+ * Copy From Here
+
 add_action('init', 'embler_register_custom_pattern_NAME');
 
-function embler_register_custom_pattern_NAME()
-{
+function embler_register_custom_pattern_NAME() {
     register_block_pattern('embler-patterns/template-slug', array(
         'title' => __('TemplateName', 'embler-patterns') ,
         'description' => _x('TemplateDescription', 'Block pattern description', 'embler-patterns') ,
@@ -16,4 +18,7 @@ function embler_register_custom_pattern_NAME()
             'embler-network'
         ) ,
     ));
-*/
+}
+
+ * To Here
+ */
